9b85869142168c6630ed7116d19d2cf7d3992e48
Jaime Malingre <jaimemalingre@gmail.com>
2023-10-24 21:55:01 +0200

    Hola Android

--- app/src/main/java/com/example/holausuario/MainActivity.java
+++ app/src/main/java/com/example/holausuario/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.holausuario;
 
-import androidx.appcompat.app.AppCompatActivity;
+//Importamos las clases correspondientes.
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,27 +13,35 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Variables
     Button botonSaludo;
 
+    //Funcion onCreate
+    //Método que es el primero que se llama cuando la actividad está a punto de ser mostrada al usuario.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Vistas NameBoton y BotonSaludo
         TextView NameBoton = findViewById(R.id.nameText);
         botonSaludo = findViewById(R.id.buttonSaludo);
 
+        //Configuraramos una barra de herramientas (toolbar)
         Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
 
 
+        //Creamos el clicklistener para el boton saludo
         botonSaludo.setOnClickListener(new View.OnClickListener() {
 
+            //Aqui manejamos los eventos de clic en las vistas.
             @Override
             public void onClick(View v) {
                 Bundle nombreDatos = new Bundle();
                 nombreDatos.putString("nombre",NameBoton.getText().toString());
 
+                //La unimos el mainPrincipal con el main numero 2
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 intent.putExtras(nombreDatos);
                 startActivity(intent);
--- app/src/main/java/com/example/holausuario/MainActivity2.java
+++ app/src/main/java/com/example/holausuario/MainActivity2.java
@@ -12,27 +12,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    //Variables Main2
     TextView nombreDos;
     ImageView fotomono;
     @Override
+    //Inicializamos la actividad en la interfaz
     protected void onCreate(Bundle savedInstanceState) {
+        //Llamamos a oncreate para que la inicialice correctamente
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        //Vista toolbar el el diseño (design)
         Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
-
+        //Obtiene la instacia de la barra de accion
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
 
+        //Buscan los identificados de las variables.
         nombreDos = findViewById(R.id.nombreDos);
         fotomono = findViewById(R.id.fotomono);
 
         Bundle nombreDatos = getIntent().getExtras();
         String nombreP = nombreDatos.getString("nombre");
 
+        //Saludamos con el nombre escrito por el usuario
         nombreDos.setText("Hola "+ nombreP+ "!");
 
+        //Si el nombre es igual a mono, no aparecera una foto de un pono en modo Visible
         if (nombreP != null && nombreP.equalsIgnoreCase("mono")) {
             fotomono.setImageResource(R.drawable.mono);
             fotomono.setVisibility(View.VISIBLE); // Muestra la foto si el nombre es "mono"
